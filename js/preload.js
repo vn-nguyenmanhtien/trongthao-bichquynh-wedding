71b3c1bab74504676c48d043cb35071dfe8eeea3
root <root@ITS-TIENNM04.ad.sungroup.com.vn>
2024-11-26 19:59:56 +0700

    change bg color

--- js/preload.js
+++ js/preload.js
@@ -1,7 +1,7 @@
 
 // PRELOAD
-$('body').attr('style', 'overflow:hidden')
+$('body').attr('style', 'overflow:hidden; background-color: #ff00000f')
 $(window).on("load", function () {
-  $('body').attr('style', 'overflow-y: scroll;')
+  $('body').attr('style', 'overflow-y: scroll; background-color: #ff00000f')
   $('.loader').delay(800).fadeOut(400)
 })
